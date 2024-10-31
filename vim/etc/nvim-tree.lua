8ec837be3f9cc73fe4865ac4423221934ddeec52
root <root@statizarm-dev.sas.yp-c.yandex.net>
2024-10-31 20:23:52 +0300

    save

--- vim/etc/nvim-tree.lua
+++ vim/etc/nvim-tree.lua
@@ -1,16 +1,9 @@
 require'nvim-tree'.setup {
   disable_netrw       = true,
   hijack_netrw        = true,
-  open_on_setup       = false,
-  ignore_ft_on_setup  = {},
-  auto_close          = false,
   open_on_tab         = false,
   hijack_cursor       = false,
   update_cwd          = false,
-  update_to_buf_dir   = {
-    enable = true,
-    auto_open = true,
-  },
   diagnostics = {
     enable = false,
     icons = {
@@ -39,15 +32,8 @@ require'nvim-tree'.setup {
     timeout = 500,
   },
   view = {
-    width = 30,
-    height = 30,
-    hide_root_folder = false,
+    width = 40,
     side = 'left',
-    auto_resize = false,
-    mappings = {
-      custom_only = false,
-      list = {}
-    },
     number = false,
     relativenumber = false,
     signcolumn = "yes"
